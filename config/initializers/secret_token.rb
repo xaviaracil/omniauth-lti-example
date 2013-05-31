958964b20a9b08d66db308d881b4a37c62e58000
Xavi Aracil <xavi@xadsolutions.com>
2013-05-31 13:06:32 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OmniauthLtiExample::Application.config.secret_token = 'd28b8cf244e10ee07aa07f92cd2e50268dc7b58fc3dd2a3663923a1c175dda64d6368ee562faea9fdd6842853c74a56904d81ec721c36c87d615ad658f7c497b'
